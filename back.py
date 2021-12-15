74634633e130da2aa6bb7954b00fd19fcd85c959
zikirillayev <zikirillayev1@gmail.com>
2021-12-15 23:41:56 +0500

    boolean

---
+++ back.py
@@ -0,0 +1,117 @@
+# # task 1
+# a  = 3
+# if a > 0:
+#     print(True)
+# else:
+#     print(False)
+
+# # task 2
+# a  = 3
+# if a % 2 ==0:
+#     print(True)
+# else:
+#     print(False)
+
+# # task 3
+# a  = 3
+# if a % 2 != 0:
+#     print(True)
+# else:
+#     print(False)
+
+# # task 4
+# a = 3
+# b = 2
+# if a > 2 and b < 3:
+#     print(True)
+# else:
+#     print(False)
+
+# # task 5
+# a = 3
+# b = 2
+# if a >= 0 and b < -2:
+#     print(True)
+# else:
+#     print(False)
+
+# task 6
+# a = 1
+# b = 2
+# c = 3
+# if a <= b and b <= c:
+#     print(True)
+# else:
+#     print(False)
+
+# task 7
+# a = 1
+# b = 2
+# c = 3
+# if a < b and b < c:
+#     print(True)
+# else:
+#     print(False)
+
+# # task 8
+# a = 2
+# b = 2
+# if a % 2 == 0 and b % 2 == 0:
+#     print(True)
+# else:
+#     print(False)
+
+# task 9 
+# a = 2
+# b = 2
+# if a % 2 != 0 or b % 2 != 0:
+#     print(True)
+# else:
+#     print(False)
+
+# task 10 
+# a = 2
+# b = 2
+# if a % 2 != 0 or b % 2 != 0:
+#     print(True)
+# else:
+#     print(False)
+
+# task 11
+# a = 3
+# b = 1
+# if a % 2 != 0 and b % 2 != 0:
+#     print(True)
+# elif a % 2 == 0 and b % 2 == 0:
+#     print(True)
+# else:
+#     print(False)
+
+# task 12
+# a = 3
+# b = -1
+# c = 1
+# if a > 0 and b > 0 and c > 0:
+#     print(True)
+# else:
+#     print(False)
+
+# task 13
+# a = -2
+# b = -1
+# c = 2
+# if a > 0 or b > 0 or c > 0:
+#     print(True)
+# else:
+#     print(False)
+
+# task 14
+# check = []
+# a = 3
+# b = -1
+# c = -11   
+# if a > 0 and b > 0 and c > 0:
+#     print(False)
+# else:
+#     print(True)
+
